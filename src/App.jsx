f0230cb99993b157caad8d608f274b8b35d5ae0c
purvapawar06 <purva.p2002@gmail.com>
2022-12-28 19:06:28 -0600

    cleaned up starter code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>To Do list</h1>
+      <p>This is a to do list app.</p>
+    </div>
+  );
+}
+
+export default App;
